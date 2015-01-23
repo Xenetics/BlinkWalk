470e61c75801b5c7be2fa0c4d541ee4fc4fa9927
Xenetics <xenetics@live.com>
2015-01-23 01:19:14 -0500

    Room Prephabs/Player control/started terrain manager

---
+++ Assets/Scripts/Managers/TerrainManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainManager : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject currentChunk;
+    [SerializeField]
+    private float speed = 5f;
+    private float chunkDonePoint = -16f;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	    //put if game playing && not paused
+        currentChunk.transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if(currentChunk.transform.position.x < chunkDonePoint)
+        {
+            //put chunk back in pool
+            //add new chunk to end of train
+        }
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject entity;
+    [SerializeField]
+    private float jumpForce = 1;
+    private bool isAlive = true;
+    private bool grounded = true;
+
+    private static PlayerController instance = null;
+    public static PlayerController Instance { get { return instance; } }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        if (isAlive)
+        {
+            if (grounded)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    entity.rigidbody2D.AddForce(new Vector2(0, jumpForce));
+                    grounded = false;
+                }
+            }
+            else
+            {
+
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        grounded = true;
+    }
+
+    public void Reset()
+    {
+
+    }
+}
